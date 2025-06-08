1dd8b490165e187a2c85f2616105ffdd401c276d
chosmh5 <154610809+chosmh5@users.noreply.github.com>
2025-06-08 21:28:41 +0900

    Update CustomCode.tsx
    Signed-off-by: chosmh5 <154610809+chosmh5@users.noreply.github.com>

--- components/CustomCode.tsx
+++ components/CustomCode.tsx
@@ -1,10 +1,1 @@
-import { Code } from 'react-notion-x'
-import FadeInWhenVisible from './FadeInWhenVisible'
 
-export default function CustomCode(props: any) {
-  return (
-    <FadeInWhenVisible>
-      <Code {...props} />
-    </FadeInWhenVisible>
-  )
-}
